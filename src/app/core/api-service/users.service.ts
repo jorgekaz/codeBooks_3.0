fcd7757b22746b55b09e7ab35d2dcf16440b3168
Juan <genova.juan@alumnos.mdp.utn.edu.ar>
2023-10-27 20:11:43 -0300

    user service login

--- src/app/core/api-service/users.service.ts
+++ src/app/core/api-service/users.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { User } from '../Modelos';
-import { CookieService } from 'ngx-cookie-service';
-import { v4 as uuidv4 } from 'uuid';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private apiService: ApiService, private cookies: CookieService) { }
+  constructor(private apiService: ApiService) { }
 
   getUsers(): Promise<User[]>{
     return new Promise((res, rej) => {
       this.apiService.getObservableUsers().subscribe({
+        next: data => res(data),
+        error: error => rej(error)
+      })
+    })
+  }
+
+  getUserById(id: number): Promise<User>{
+    return new Promise((res, rej) => {
+      this.apiService.getObservableUser(id).subscribe({
         next: data => res(data),
         error: error => rej(error)
       })
@@ -29,10 +38,21 @@ export class UsersService {
     })
   }
 
-  setToken() {
-    this.cookies.set("X-Auth-Token", uuidv4());
+  userLoguin(usuario: string, password: string): Promise<any> {
+    return new Promise((resp, rej) => {
+      this.apiService.userLoguin(usuario, password).subscribe({
+        next: data => resp(data),
+        error: error => rej(error)
+      })
+    })
   }
-  getToken() {
-    return this.cookies.get("X-Auth-Token");
+
+  updateUser(datosFormulario: string, idUsuario: number){
+    return new Promise((resp, rej) => {
+      this.apiService.updateUser(datosFormulario, idUsuario).subscribe({
+        next: data => resp(data),
+        error: error => rej(error)
+      })
+    })
   }
 }
