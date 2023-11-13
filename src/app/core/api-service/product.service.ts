07ac879259aa345f4712dc0d9544b26ac870d5d1
Jorge Karp <jorgekarp@gmail.com>
2023-11-13 20:26:21 -0300

    Arreglo Bugs
    Se arreglo un problema que permitía sacar los libros de la lista de detalles de la compra y se evitó guardar en el objeto la variable confirm password cuando se registra un usuario nuevo.

--- src/app/core/api-service/product.service.ts
+++ src/app/core/api-service/product.service.ts
@@ -3,45 +3,48 @@ import { ApiService } from './api.service';
 import { Book } from '../Modelos';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private apiService: ApiService) {}
 
-  constructor(private apiService: ApiService) { }
-
-  getProducts(): Promise<Book[]>{
-    return new Promise((res, rej) =>{
+  getProducts(): Promise<Book[]> {
+    return new Promise((res, rej) => {
       this.apiService.getCarrito('').subscribe({
-         next: data => res(data),
-         error: error => rej(error)
-      })
-    })
+        next: (data) => res(data),
+        error: (error) => rej(error),
+      });
+    });
   }
 
-  getProductById(id: number): Promise<Book>{
+  getProductById(id: number): Promise<Book> {
     return new Promise((res, rej) => {
       this.apiService.getObservableBook(id).subscribe({
-        next: data => res(data),
-        error: error => rej(error)
-      })
-    })
+        next: (data) => res(data),
+        error: (error) => rej(error),
+      });
+    });
   }
 
-  searchProducts(search: string): Promise<Book[]>{
+  searchProducts(search: string): Promise<Book[]> {
     return new Promise((res, rej) => {
       this.apiService.getCarrito(search).subscribe({
-        next: data => res(data),
-        error: error => rej(error)
-      })
-    })
+        next: (data) => res(data),
+        error: (error) => rej(error),
+      });
+    });
   }
 
-  guardarCarrito(idUsuario: number, cart: number[], costoTotal: number): Promise<any>{
+  guardarCarrito(
+    idUsuario: number,
+    cart: number[],
+    costoTotal: number
+  ): Promise<any> {
     return new Promise((res, rej) => {
       this.apiService.guardarCarrito(idUsuario, cart, costoTotal).subscribe({
-        next: data => res(data),
-        error: error => rej(error)
-      })
-    })
+        next: (data) => res(data),
+        error: (error) => rej(error),
+      });
+    });
   }
 }
